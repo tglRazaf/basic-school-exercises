e9dcc709207bea2bb176aef09880208e2fa34930
stephano <stephanorazafindramena@gmail.com>
2022-01-11 14:09:12 +0300

    new repos git

--- java/polymorphisme/Liste.java
+++ java/polymorphisme/Liste.java
@@ -1,7 +1,13 @@
 package polymorphisme;
 
+
+
 public interface Liste {
     public void ajout(int nouvVal);
     public int extraction();
     public boolean estVide();
 }
+
+
+//les class qui implemente cet interface est da le dossier /src/lifo_fifo et aussi le fichier FileDePriorieFinal dans /src
+//Ici on utilise interface car on veut que chaque methode (abstract) a sa propre implementation dans chaque class fille
---
+++ js/ts/app.ts
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+class Player {
+    name:String;
+    age:number;
+    constructor(parameters) {
+        
+    }
+    printName = ():String=>{
+        return this.name;
+    }
+}
